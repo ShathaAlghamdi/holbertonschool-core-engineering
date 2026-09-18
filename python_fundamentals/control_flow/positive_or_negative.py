348c3fcdafc03ddc008810162526517ed04a0ce8
Shatha <61396210+ShathaAlghamdi@users.noreply.github.com>
2026-09-18 21:40:57 +0300

    Task 1 Python - Flujo de control

---
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -0,0 +1,7 @@
+def check_positive_or_negative(number):
+    if number>0:
+        return f"{number} the number is positive"
+    elif number == 0:
+        return f"{number} the number is zero"
+    else:
+        return f"{number} the number is negative"
--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
-language = "Python"
-version = 3
-pi_approx = 22 / 7
-computation_valid = version == 3
+# language = "Python"
+# version = 3
+# pi_approx = 22 / 7
+# computation_valid = version == 3
 
-print(f"Language: {language}")
-print(f"Version: {version}")
-print(f"Pi approx: {pi_approx:.2f}")
-print(f"Computation valid: {computation_valid}")
+# print(f"Language: {language}")
+# print(f"Version: {version}")
+# print(f"Pi approx: {pi_approx:.2f}")
+# print(f"Computation valid: {computation_valid}")
+
+def pig_latin_sentence(sentence):
+    words = sentence.split()
+
+    result = []
+
+    for word in words:
+        result.append(word[1:] + word[0] + "ay")
+
+    return " ".join(result)
+
+print(pig_latin_sentence("hello world python"))
